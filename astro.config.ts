586216c2ce4e776ba0638a0958bad428e8aea7de
christos.sextos <christos.sextos@gmail.com>
2024-12-20 16:04:27 +0000

    changes

--- astro.config.ts
+++ astro.config.ts
@@ -43,7 +43,8 @@ export default defineConfig({
           'voice-presentation',
           'business-contact',
           'database',
-        ],
+         ],
+        mdi: ['*']
       },
     }),
 
